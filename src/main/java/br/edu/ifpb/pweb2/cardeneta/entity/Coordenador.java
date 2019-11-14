6276ae76e56e53c2f14e77da30700d44ee73b2bf
samuel <samueldcabral@gmail.com>
2019-11-13 21:42:45 -0300

    entidade coordenador com anotacoes corretas

--- src/main/java/br/edu/ifpb/pweb2/cardeneta/entity/Coordenador.java
+++ src/main/java/br/edu/ifpb/pweb2/cardeneta/entity/Coordenador.java
@@ -1,5 +1,10 @@
 package br.edu.ifpb.pweb2.cardeneta.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_COORDENADOR")
 public class Coordenador extends Professor{
 	
 	private Boolean ativo;
