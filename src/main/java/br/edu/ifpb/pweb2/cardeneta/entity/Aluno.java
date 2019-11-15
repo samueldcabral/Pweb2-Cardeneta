3d425ae8234af50a1bbc194a18718b377a1070bf
Renatha <renatha.nvictor@gmail.com>
2019-11-15 12:15:24 -0300

    alteração nos relacionamentos

--- src/main/java/br/edu/ifpb/pweb2/cardeneta/entity/Aluno.java
+++ src/main/java/br/edu/ifpb/pweb2/cardeneta/entity/Aluno.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -26,6 +27,9 @@ public class Aluno {
 	
 	@OneToMany
 	private List<Nota> notas;
+	
+	@OneToOne
+	private Usuario usuario;
 
 	public List<Turma> getTurmas() {
 		return turmas;
