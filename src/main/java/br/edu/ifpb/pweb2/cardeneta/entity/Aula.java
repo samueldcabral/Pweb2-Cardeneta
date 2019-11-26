d087e4293a3c2f8313314f34b4d3f09733000dcd
Adriano Ney Nascimento do Amarala <adriano.amaral147@gmail.com>
2019-11-26 19:38:28 -0300

    alteração data e serializable

--- src/main/java/br/edu/ifpb/pweb2/cardeneta/entity/Aula.java
+++ src/main/java/br/edu/ifpb/pweb2/cardeneta/entity/Aula.java
@@ -1,22 +1,20 @@
 package br.edu.ifpb.pweb2.cardeneta.entity;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Entity
 @Table(name="TB_AULA")
-public class Aula {
-	
+public class Aula implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String assunto;
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	private Date data;
 	
 	@ManyToOne
